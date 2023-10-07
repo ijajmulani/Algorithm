55bac43bb30a52551ba245664ae7a7357bd520d9
IJAJ MULANI <ijajmulani786@gmail.com>
2023-10-07 13:38:18 +0530

    Update sorting.go

--- sorting.go
+++ sorting.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func selectionSort(arr []int) {
@@ -21,24 +23,112 @@ func selectionSort(arr []int) {
 
 func bubbleSort(arr []int) {
 	n := len(arr)
-	
+
 	for i := 0; i < n-1; i++ {
 		swap := false
 		for j := 0; j < n-i-1; j++ {
 			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]	
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 				swap = true
 			}
 		}
-		fmt.Println(i)
 		if !swap {
 			break
 		}
 	}
 }
 
+func insertionSort(arr []int) {
+	n := len(arr)
+	for i := 1; i < n; i++ {
+		j := i - 1
+		key := arr[i]
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+}
+
+func mergeSort(arr []int) []int {
+	n := len(arr)
+
+	if n <= 1 {
+		return arr
+	}
+	mid := n / 2
+
+	l := mergeSort(arr[:mid])
+	r := mergeSort(arr[mid:])
+
+	i, j := 0, 0
+	final := []int{}
+	for i < len(l) && j < len(l) {
+		if l[i] < r[j] {
+			final = append(final, l[i])
+			i++
+		} else {
+			final = append(final, r[j])
+			j++
+		}
+	}
+
+	for ; i < len(l); i++ {
+		final = append(final, l[i])
+	}
+	for ; j < len(r); j++ {
+		final = append(final, r[j])
+	}
+	return final
+}
+
 func main() {
-	arr := []int{1, 2, 3, 6, 5, 4, 7, 8, 9}
+	rand.Seed(time.Now().Unix())
+	
+	arr := rand.Perm(10000)
+	start := time.Now()
+	selectionSort(arr)
+	fmt.Printf(" selectionSort took at time %v\n", time.Since(start))
+	
+	arr=nil
+	arr = rand.Perm(10000)
+	start = time.Now()
 	bubbleSort(arr)
-	fmt.Println(arr)
+	fmt.Printf(" bubbleSort took at time %v\n", time.Since(start))
+	
+	arr=nil
+	arr = rand.Perm(10000)
+	start = time.Now()
+	insertionSort(arr)
+	fmt.Printf(" insertionSort took at time %v\n", time.Since(start))
+	
+	arr=nil
+	arr = rand.Perm(10000)
+	start = time.Now()
+	quickSort(arr, 0, len(arr)-1)
+	fmt.Printf(" quickSort took at time %v\n", time.Since(start))
+}
+
+func partition(arr []int, low, high int) int {
+	key := arr[high]
+	j := low
+	for i := low; i < high; i++ {
+		if arr[i] < key {
+
+			arr[i], arr[j] = arr[j], arr[i]
+			j++
+		}
+	}
+	arr[j], arr[high] = arr[high], arr[j]
+	return j
+}
+
+func quickSort(arr []int, low, high int) {
+	if low >= high {
+		return
+	}
+	p := partition(arr, low, high)
+	quickSort(arr, low, p-1)
+	quickSort(arr, p+1, high)
 }
