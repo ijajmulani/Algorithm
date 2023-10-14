e37910575f2afeca3432095cc778884707dbf81d
IJAJ MULANI <ijajmulani786@gmail.com>
2023-10-14 19:25:38 +0530

    Update heap-data-structure.go

--- heap-data-structure.go
+++ heap-data-structure.go
@@ -7,87 +7,92 @@ import (
 	"math"
 )
 
-type heap struct {
-	maxSize int
-	data    []int
+type minHeap struct {
+	data []int
 }
 
-func (h *heap) parent(k int) int {
-	return int(math.Floor(float64((k - 1) / 2)))
+func (h *minHeap) Parent(i int) int {
+	return (i - 1) / 2
 }
 
-func (h *heap) lChildIndex(k int) int {
-	return 2*k + 1
-}
-func (h *heap) rChildIndex(k int) int {
-	return 2*k + 2
+func (h *minHeap) LeftNode(i int) int {
+	return 2*i + 1
 }
 
-func (h *heap) insert(v int) {
-	if len(h.data) == h.maxSize {
-		return
-	}
+func (h *minHeap) RightNode(i int) int {
+	return 2*i + 2
+}
+
+func (h *minHeap) Insert(v int) {
 	h.data = append(h.data, v)
-	i := len(h.data) - 1
-	for i != 0 && h.data[h.parent(i)] < h.data[i] {
-		h.data[h.parent(i)], h.data[i] = h.data[i], h.data[h.parent(i)]
-		i = h.parent(i)
+	n := len(h.data) - 1
+	for n > 0 && h.data[h.Parent(n)] > h.data[n] {
+		h.data[h.Parent(n)], h.data[n] = h.data[n], h.data[h.Parent(n)]
+		n = h.Parent(n)
 	}
 }
 
-func (h *heap) increaseKey(i int) {
-	h.data[i] = math.MaxInt32
-	for i != 0 && h.data[h.parent(i)] < h.data[i] {
-		h.data[h.parent(i)], h.data[i] = h.data[i], h.data[h.parent(i)]
-		i = h.parent(i)
+func (h *minHeap) MinHeapify(i int) {
+	smallest := i
+	left := h.LeftNode(i)
+	right := h.RightNode(i)
+	if left < len(h.data) && h.data[left] < h.data[smallest] {
+		smallest = left
+	}
+	if right < len(h.data) && h.data[right] < h.data[smallest] {
+		smallest = right
+	}
+	if i != smallest {
+		h.data[smallest], h.data[i] = h.data[i], h.data[smallest]
+		h.MinHeapify(smallest)
 	}
 }
 
-func (h *heap) heapify(i int) {
-
-	lc := h.lChildIndex(i)
-	rc := h.rChildIndex(i)
-	largest := i
-	if lc < len(h.data) && h.data[lc] > h.data[largest] {
-		largest = lc
-	}
-	if rc < len(h.data) && h.data[rc] > h.data[largest] {
-		largest = rc
-	}
-	if largest != i {
-		h.data[i], h.data[largest] = h.data[largest], h.data[i]
-	}
-
-}
-
-func (h *heap) removeMax() {
-	h.data[0] = h.data[len(h.data)-1]
+func (h *minHeap) ExtractMin() {
+	root := 0
+	last := len(h.data) - 1
+	h.data[root], h.data[last] = h.data[last], h.data[root]
 	h.data = h.data[:len(h.data)-1]
-	h.heapify(0)
-
+	h.MinHeapify(0)
 }
 
-func (h *heap) delete(i int) {
-	if len(h.data) == 0 || (i == 1 && len(h.data) == 1) {
-		h.data = nil
-		return
+func (h *minHeap) DeleteKey(i int) {
+	h.data[i] = math.MinInt
+	for i >= 0 && h.data[i] < h.data[h.Parent(i)] {
+		h.data[h.Parent(i)], h.data[i] = h.data[i], h.data[h.Parent(i)]
+		i = h.Parent(i)
 	}
+	h.ExtractMin()
+}
 
-	if i <= len(h.data)-1 {
-		h.increaseKey(i)
-		h.removeMax()
+func (h *minHeap) BuildHeap() {
+	i := h.Parent(len(h.data) - 1)
+	for ; i >= 0; i-- {
+		h.MinHeapify(i)
 	}
+}
+func (h *minHeap) Print() {
+	fmt.Println(h.data)
 }
 
 func main() {
-	h := &heap{
-		maxSize: 15,
-	}
-	h.insert(4)
-	h.insert(1)
-	h.insert(3)
-	h.insert(7)
-	fmt.Println(h.data, len(h.data))
-	h.delete(2)
-	fmt.Println(h.data)
+	h := new(minHeap)
+	// [10 20 15 40 50 100 25 45]
+
+	h.Insert(40)
+	h.Insert(20)
+	h.Insert(30)
+	h.Insert(35)
+	h.Insert(25)
+	h.Insert(80)
+	h.Insert(32)
+	h.Insert(100)
+	h.Insert(70)
+	h.Insert(60)
+
+	//h.ExtractMin()
+	h.Print()
+	//h.DeleteKey(3)
+	h.BuildHeap()
+	h.Print()
 }
